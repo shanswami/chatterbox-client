04e5650b4b9aa9b5f83504a2673a165f1af0d469
shanswami <shan.swami@gmail.com>
2014-09-22 15:45:23 -0700

    moderate progress. added our basic functions, implemented working get and post commands. working on the Dom elements now

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,47 @@
-// YOUR CODE HERE:
+var app = {
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  init: function() {
 
+  },
+  send: function(message) {
+    $.ajax({
+      // always use this url
+      url: this.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+  fetch: function() {
+    $.ajax({
+      // always use this url
+      url: this.server,
+      type: 'GET',
+      data: 'data',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message received');
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to get message');
+      }
+    });
+  },
+  clearMessages: function() {
+
+    $('#chats').empty();
+  },
+  addMessage: function(message) {
+    console.log($('#chats'))
+    $('#chats').html(message.text);
+  }
+
+};
