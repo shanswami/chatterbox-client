b01e5eec61c4096b41ca80625248709f482547cc
shanswami <shan.swami@gmail.com>
2014-09-22 20:22:01 -0700

    much more progress made. got our timestamps, refresh, and duplicate issues all sorted out.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,14 @@
 var app = {
   server: 'https://api.parse.com/1/classes/chatterbox',
   init: function() {
-
+    app.room = 'Default';
+    var that = this;
+    // setInterval(function(){
+    //   that.fetch();
+    // }, 2000);
   },
   send: function(message) {
+
     $.ajax({
       // always use this url
       url: this.server,
@@ -21,13 +26,34 @@ var app = {
   },
   fetch: function() {
     $.ajax({
-      // always use this url
       url: this.server,
       type: 'GET',
-      data: 'data',
+      data: {
+        order: '-createdAt'
+      },
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message received');
+        var received = data.results;
+        for (var i = received.length - 1; i > 0; i--){
+          var theMessage = received[i].text;
+          var name = received[i].username;
+          var room = received[i].roomname;
+          var lastTime = new Date($('ul').last().text().slice($('ul').last().text.length - 25));
+          var currentTime = new Date(received[i].createdAt);
+          if ($('ul').length > 15){
+            console.log('yes');
+            if (currentTime.getTime() > lastTime.getTime()){
+              if (theMessage !== undefined && name !== undefined && name.charAt(0) !== '<' && theMessage.charAt(0) !== '<'){
+                $('#chats').append('<ul>'+name+': '+theMessage+'_______'+ received[i].createdAt+'</ul>');
+                $('ul').first().remove();
+              }
+            }
+          } else {
+            if (theMessage !== undefined && name !== undefined && name.charAt(0) !== '<' && theMessage.charAt(0) !== '<'){
+             $('#chats').append('<ul>'+name+': '+theMessage+'_______'+ received[i].createdAt+'</ul>');
+            }
+          }
+        }
       },
       error: function (data) {
         // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -36,12 +62,29 @@ var app = {
     });
   },
   clearMessages: function() {
-
     $('#chats').empty();
   },
   addMessage: function(message) {
-    console.log($('#chats'))
-    $('#chats').html(message.text);
+    $('#chats').append('<ul>'+message+'</ul>');
+  },
+  addRoom: function(room) {
+    $('#roomSelect').append('<ul>'+room+'</ul>');
+  },
+  addFriend: {
+    called: true,
+    restore: function() {}
   }
 
 };
+
+app.init();
+$('.send').on('click', function() {
+  console.log("stop clicking me.");
+  // var message = {
+  //   'username': 'this.username',
+  //   'text': 'this.text',
+  //   'roomname': 'Default'
+  // };
+  // app.send(message);
+});
+
